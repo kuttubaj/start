ca0df6d4a477746850edfbad8bce797e22976b48
user <iskakovkuttuubaj0207@gmail.com>
2024-03-13 15:37:46 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android/codelabs/paging/data/ArticlePagingSource.kt
@@ -0,0 +1,45 @@
+package com.example.android.codelabs.paging.data
+
+import Article
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import kotlinx.coroutines.delay
+import java.lang.Math.max
+import java.time.LocalDateTime
+
+const val START_KEY = 0
+private const val LOAD_DELAY_MILLIS = 3_000L
+
+private val firstArticleCreatedTime = LocalDateTime.now()
+
+class ArticlePagingSource : PagingSource<Int, Article>() {
+
+    private fun ensureValidKey(key:Int ) = max(START_KEY,key)
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
+        val start = params.key ?: START_KEY
+        val range = start.until(start + params.loadSize)
+        if (start != START_KEY) delay(LOAD_DELAY_MILLIS)
+        return LoadResult.Page(
+            data = range.map { number->
+                Article(
+                    id = number,
+                    title = "Article $number",
+                    description = "This describes article $number",
+                    created = firstArticleCreatedTime.minusDays(number.toLong())
+                )
+            },
+            prevKey = when(start){
+                START_KEY -> null
+                else -> ensureValidKey(key = range.first - params.loadSize)
+            },
+            nextKey = range.last +1
+        )
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
+        val anchorPosition = state.anchorPosition ?: return null
+        val article = state.closestItemToPosition(anchorPosition) ?: return null
+        return ensureValidKey(key = article.id - (state.config.pageSize / 2))
+    }
+}
